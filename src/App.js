8e9e008d8aab1420f0c6edf5cc4bd2465b4126ee
Pouyan <pouyan.zarbafian@hotmail.com>
2022-08-14 14:31:54 -0400

    add the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import CardList from "./components/CardList";
+import SearchBox from "./components/SearchBox";
+import Scroll from "./components/Scroll";
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [robots, setRobotos] = useState([]);
+  const onSearchChange = (e) => {
+    setSearchField(e.target.value);
+  };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setRobotos(data));
+  }, [robots]);
+
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  return !robots.length ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <SearchBox onSearchChange={onSearchChange} />
+      <Scroll>
+        <CardList robots={filteredRobots} />;
+      </Scroll>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/CardList.js
@@ -0,0 +1,14 @@
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((robot, i) => (
+        <Card key={i} id={robot.id} name={robot.name} email={robot.email} />
+      ))}
+      ;
+    </div>
+  );
+};
+
+export default CardList;

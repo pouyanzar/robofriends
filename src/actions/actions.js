e7fd33f9fe061d7f3ade41bd8c1e713f04235aff
Pouyan <pouyan.zarbafian@hotmail.com>
2024-02-06 14:59:38 -0500

    update the project structure

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,7 +3,7 @@ import {
   REQUEST_ROBOTS_PENDING,
   REQUEST_ROBOTS_SUCCESS,
   REQUEST_ROBOTS_FAILED,
-} from "../containers/constants";
+} from "../constants/constants";
 
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -3,7 +3,7 @@ import {
   REQUEST_ROBOTS_PENDING,
   REQUEST_ROBOTS_SUCCESS,
   REQUEST_ROBOTS_FAILED,
-} from "../containers/constants";
+} from "../constants/constants";
 
 const initialStateSearch = {
   searchField: "",
